3afc4e6f5a6045213a0e7f59d68f940c9b47368d
qinjiahao666 <656673477@qq.com>
2019-11-21 22:09:16 +0800

    update

--- python/python_space.py
+++ python/python_space.py
@@ -1,41 +1,58 @@
-# import numpy as np
-# # from matplotlib import pyplot as plt
-# #
-# # x = np.arange(1, 11)
-# # y = 2 * x + 5
-# #
-# # plt.title("matplotlib demo")
-# # plt.xlabel("X")
-# # plt.ylabel("Y")
-# #
-# # plt.plot(x, y, "ob")
-# # plt.show()
+import numpy as np
+from matplotlib import pyplot as plt
 
-# for value in range(1, 5):
-#     print(value)
+x = np.arange(1, 11)
+y = 2 * x + 5
+
+plt.title("matplotlib demo")
+plt.xlabel("X")
+plt.ylabel("Y")
+
+plt.plot(x, y, "ob")
+plt.show()
+
+for value in range(1, 5):
+    print(value)
 
 numbers = list(range(2, 11, 2))
 print(numbers)
 
-# bicycles = ['trek', 'cannondale', 'specialized']
-# for bicycle in bicycles:
-#     print(bicycle)
+bicycles = ['trek', 'cannondale', 'specialized', 'qin']
+for bicycle in bicycles[0:3]:
+    print(bicycle)
 
-# print(bicycles)
-# bicycles.sort()
-# print(bicycles)
+print(bicycles)
+bicycles.sort()
+print(bicycles)
 
-# popped_bicycles = bicycles.pop()
-# print(popped_bicycles)
+popped_bicycles = bicycles.pop()
+print(popped_bicycles)
 
-# del bicycles[0]
-# print(bicycles)
-# bicycles.insert(0, 'ducati')
-# print(bicycles)
-# bicycles[0] = 'trak'
-# print(bicycles)
+del bicycles[0]
+print(bicycles)
+bicycles.insert(0, 'ducati')
+print(bicycles)
+bicycles[0] = 'trak'
+print(bicycles)
 
-# print(bicycles[0].title())
+print(bicycles[0].title())
 
-# message = "My first bicycle was a " + bicycles[0].title() + "."
-# print(message)
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
+
+# 元组的练习
+
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+dimensions = (400, 200)
+print(dimensions[0])
+print(dimensions[1])
+
+
+# 字典的练习
+alien_0 = {'color': 'green', 'points': '5'}
+print(alien_0)
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
