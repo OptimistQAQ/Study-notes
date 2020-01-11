d6eaa4008ec178f2980172544a25e133d95ec61e
qinjiahao666 <656673477@qq.com>
2020-01-12 00:24:36 +0800

    :memo: first commit

---
+++ 爬虫/spider_first_study.py
@@ -0,0 +1,13 @@
+import urllib.request
+
+# 提供URL/URI
+response = urllib.request.urlopen('https://www.python.org')
+
+# print(response.read().decode('utf-8'))
+
+# print(type(response))
+
+# print(response.status)
+# print(response.getheaders())
+# print(response.getheader('Server'))
+
