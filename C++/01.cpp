b4708c8143745d2538ae888cce75ca35b1b9d9e3
OptimistQAQ <656673477@qq.com>
2020-10-28 16:29:43 +0800

    add

--- C++/01.cpp
+++ C++/01.cpp
@@ -10,11 +10,74 @@
 
 using namespace std;
 
-int main()
-{
-	
+int main(){
+	int a, b, c;
+	for (int i=100; i <1000; i++){
+		a = i/100;
+		b = i % 100 /10;
+		c = i % 10;
+		if (a*a*a+b*b*b+c*c*c == i){
+			cout << i << endl;
+		}
+	}
 	return 0;
 }
+
+//int f[20190324];
+//
+//int main(){
+//	f[0]=f[1]=f[2]=1;
+//	for (int i=3; i<20190324; i++){
+//		f[i] = (f[i-1] + f[i-2] + f[i-3]) % 10000;
+//	}
+//	cout << f[20190323] << endl;
+//	return 0;
+//}
+
+
+//int main(){
+//	string str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//	char ans[5];
+//	int index = 0;
+//	int n = 2019;
+//	while(n) {
+//		int t = n % 26;
+//		n = n / 26;
+//		ans[index] = str[t-1];
+//		index++;
+//	}
+//	for (int i=index-1; i >= 0; i--){
+//		cout << ans[i];
+//	}
+//	return 0;
+//}
+
+
+
+//int check(int n){
+//	while(n){
+//		int t = n % 10;
+//		n = n /10;
+//		if(t==2 || t==0 || t==1 || t==9)
+//			return 1;
+//	}
+//	return 0;
+//}
+//
+//int main(){
+//	int n;
+//	int sum = 0;
+//	cin >> n;
+//	for (int i=1; i <= n; i++){
+//		if(check(i)){
+//			sum += i;
+//		}
+//	}
+//	cout << sum << endl;
+//	return 0;
+//} 
+
+
 
 //const int maxn = 1004;
 //const double MAX = 1e9;
